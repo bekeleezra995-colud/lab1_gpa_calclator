be5d8980fbaa28cc9980a4a410088d7d9447cb6f
ethiogech622-byte <ethiogech622@gmail.com>
2025-11-30 09:37:34 +0200

    Implementing Course Service Functions

--- gpacalculator/courses/CoursesService.py
+++ gpacalculator/courses/CoursesService.py
@@ -1,32 +1,40 @@
-students = []   # list of dicts
+courses = []  # list of dicts: {"code": "CS101", "title": "Intro", "credit": 3}
 
-def register_student():
-    sid = input("Enter student ID: ")
-    name = input("Enter student name: ")
-
-    students.append({"id": sid, "name": name})
-    print("Student registered successfully!")
-
-def list_students():
-    if not students:
-        print("No students registered yet!")
-        return
-    print("\n--- Registered Students ---")
-    for s in students:
-        print(f"ID: {s['id']} | Name: {s['name']}")
-
-def student_menu():
+def register_course():
+    code = input("Enter course code: ")
+    title = input("Enter course title: ")
     while True:
-        print("\n--- Student Menu ---")
-        print("1. Register Student")
-        print("2. List Students")
+        try:
+            credit = int(input("Enter course credit: "))
+            break
+        except ValueError:
+            print("Invalid credit! Please enter a number.")
+
+    courses.append({"code": code, "title": title, "credit": credit})
+    print("Course registered successfully!")
+
+def list_courses():
+    if not courses:
+        print("No courses registered yet!")
+        return
+    print("\n--- Registered Courses ---")
+    print(f"{'Code':<10} {'Title':<20} {'Credit':<5}")
+    print("-" * 40)
+    for c in courses:
+        print(f"{c['code']:<10} {c['title']:<20} {c['credit']:<5}")
+
+def course_menu():
+    while True:
+        print("\n--- Course Menu ---")
+        print("1. Register Course")
+        print("2. List Courses")
         print("3. Back")
         choice = input("Enter choice: ")
 
         if choice == "1":
-            register_student()
+            register_course()
         elif choice == "2":
-            list_students()
+            list_courses()
         elif choice == "3":
             break
         else:
